2c39518f5f9748b01b6db4caf4d80b8a42019dc2
Josh Field <joshfield999@gmail.com>
2024-03-03 14:45:24 +1100

    Update convert_obj_glb.py

--- convert_obj_glb.py
+++ convert_obj_glb.py
@@ -5,6 +5,27 @@ import bpy
 import sys
 import os
 
+def deleteAllObjects():
+    """
+    Deletes all objects in the current scene
+    """
+    deleteListObjects = ['MESH', 'CURVE', 'SURFACE', 'META', 'FONT', 'HAIR', 'POINTCLOUD', 'VOLUME', 'GPENCIL',
+                     'ARMATURE', 'LATTICE', 'EMPTY', 'LIGHT', 'LIGHT_PROBE', 'CAMERA', 'SPEAKER']
+
+    # Select all objects in the scene to be deleted:
+
+    for o in bpy.context.scene.objects:
+        for i in deleteListObjects:
+            if o.type == i:
+                o.select_set(False)
+            else:
+                o.select_set(True)
+    # Deletes all selected objects in the scene:
+
+    bpy.ops.object.delete() 
+
+
+
 # Get all args after "--"
 argv = sys.argv
 argv = argv[argv.index("--") + 1:]
@@ -18,7 +39,9 @@ files = [f for f in files if os.path.isfile(directory+'/'+f) and f.endswith('.ob
 print("Converting files:", files)
 
 for f in files:
-    bpy.ops.scene.new(type='EMPTY')
+    # reset
+    deleteAllObjects()
+    # import obj and export as glb
     obj_in = directory + '/' + f
     gltf_out = directory + '/' + f.split('.')[0] + '.glb'
     bpy.ops.wm.obj_import(filepath=obj_in, forward_axis='NEGATIVE_Z', up_axis='Y')
